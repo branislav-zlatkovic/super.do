7d777a71737278b882334a0f04bfc29fa3e59a19
Branislav Zlatkovic <branislav.n.zlatkovic@gmail.com>
2021-10-25 13:58:05 +0200

    Rename classes, packages, and restructure the project to suit the desired app requirements

--- app/src/main/java/com/example/myapplication/view/itemdetails/ItemDetailsFragment.kt
+++ app/src/main/java/com/example/myapplication/view/itemdetails/ItemDetailsFragment.kt
@@ -1,19 +1,20 @@
-package com.example.myapplication
+package com.example.myapplication.view.itemdetails
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import com.example.myapplication.databinding.FragmentFirstBinding
+import com.example.myapplication.R
+import com.example.myapplication.databinding.FragmentItemDetailsBinding
 
 /**
- * A simple [Fragment] subclass as the default destination in the navigation.
+ * A simple [Fragment] subclass as the second destination in the navigation.
  */
-class FirstFragment : Fragment() {
+class ItemDetailsFragment : Fragment() {
 
-    private var _binding: FragmentFirstBinding? = null
+    private var _binding: FragmentItemDetailsBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -24,7 +25,7 @@ class FirstFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        _binding = FragmentItemDetailsBinding.inflate(inflater, container, false)
         return binding.root
 
     }
@@ -32,8 +33,8 @@ class FirstFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonFirst.setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        binding.buttonSecond.setOnClickListener {
+            findNavController().navigate(R.id.action_item_details_to_listings)
         }
     }
 
